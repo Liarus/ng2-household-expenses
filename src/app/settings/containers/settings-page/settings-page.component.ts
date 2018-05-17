f1e8545ffa0d35fa0abd6430e117225412d8185e
Hodorek, Tomasz <tomasz.hodorek@capgemini.com>
2018-05-17 09:26:00 +0200

    styled container

--- src/app/settings/containers/settings-page/settings-page.component.ts
+++ src/app/settings/containers/settings-page/settings-page.component.ts
@@ -4,8 +4,14 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-settings-page',
   styleUrls: ['./settings-page.component.scss'],
   template: `
-    <app-expense-type-page></app-expense-type-page>
-    <app-saving-type-page></app-saving-type-page>
+    <div class="ui-g">
+      <div class="ui-g-12 ui-sm-12 ui-md-12 ui-lg-6">
+        <app-expense-type-page></app-expense-type-page>
+      </div>
+      <div class="ui-g-12 ui-sm-12 ui-md-12 ui-lg-6">
+        <app-saving-type-page></app-saving-type-page>
+      </div>
+    </div>
   `
 })
 export class SettingsPageComponent implements OnInit {
